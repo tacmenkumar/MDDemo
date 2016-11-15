ab5b2f499109fa839b6203410a064ce3d3e6cf9e
Niraj Kumar <nkumar@tacme.com>
2016-11-15 12:32:04 +0400

    code changed

--- mobile/src/main/java/tacme/com/mddemo/BaseActivity.java
+++ mobile/src/main/java/tacme/com/mddemo/BaseActivity.java
@@ -11,5 +11,7 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     public void onCreate (Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
+
+        // Code Modified
     }
 }
